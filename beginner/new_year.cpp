589d105a053a8d61a43fbe8dca96acb6e53b6c65
Ippei523 <ippei523@gmail.com>
2024-08-23 01:18:10 +0900

    AtCoderの練習

---
+++ beginner/new_year.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int after_two_days = 48;
+  int current_time;
+
+  cin >> current_time;
+
+  cout << after_two_days - current_time << endl;
+}
---
+++ practice-cpp/f_105.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // ここにプログラムを追記
+  int a, b;
+
+  cin >> a;
+  cin >> b;
+
+  cout << a + b << endl;
+}
